6f32faf46e12523a21d0e153021557f1b462de02
tardyp <tardyp@f2d1aff8-5853-0410-a2ad-4df87bb8d04b>
2008-07-31 23:08:09 +0000

    imported cortex's patches but got some problems to build python on osx..
    git-svn-id: http://wiipy.googlecode.com/svn/trunk@3 f2d1aff8-5853-0410-a2ad-4df87bb8d04b

--- cross_compile_python.sh
+++ cross_compile_python.sh
@@ -1,30 +1,26 @@
 # http://whatschrisdoing.com/blog/2006/10/06/howto-cross-compile-python-25/
 
-if [ ! -f Python2.5_xcompile.patch ]
+if [ ! -f Python-2.5.2.tar.bz2 ]
 then
-wget http://whatschrisdoing.com/~lambacck/Python2.5_xcompile.patch
+wget http://www.python.org/ftp/python/2.5.2/Python-2.5.2.tar.bz2
 fi
+rm -rf Python-2.5.2
+tar xjf Python-2.5.2.tar.bz2
+cd Python-2.5.2
 
-if [ ! -f Python-2.5.tar.bz2 ]
-then
-wget http://www.python.org/ftp/python/2.5/Python-2.5.tar.bz2
-fi
-rm -rf Python-2.5
-tar xjf Python-2.5.tar.bz2
-cd Python-2.5
-patch -p1 <../Python2.5_xcompile.patch
-
-autoconf
 
 export CC=gcc
 export CXX=g++
 
 ./configure
-make python Parser/pgen
+make python Parser/pgen || exit
 mv python hostpython
 mv Parser/pgen Parser/hostpgen
 make distclean
 
+patch -p1 <../python_2.5.2.patch
+
+autoconf
 
 
 export PATH=$DEVKITPPC/bin:$PATH
