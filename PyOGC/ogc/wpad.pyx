343d7c2d9ccc05143bb38a785c33d4e01593c84f
tardyp <tardyp@f2d1aff8-5853-0410-a2ad-4df87bb8d04b>
2008-08-02 23:45:33 +0000

    oups, wrong file
    git-svn-id: http://wiipy.googlecode.com/svn/trunk@7 f2d1aff8-5853-0410-a2ad-4df87bb8d04b

--- PyOGC/ogc/wpad.pyx
+++ PyOGC/ogc/wpad.pyx
@@ -1,111 +1,120 @@
 """
-PAD Subsystem
+WPAD Subsystem (support for wiimote)
+currently, only buttons support.
 """
 
-cdef extern from "ogc/pad.h":
-	ctypedef struct PADStatus:
-		unsigned short button
-		signed   char  stickX
-		signed   char  stickY
-		signed   char  substickX
-		signed   char  substickY
-		unsigned char  triggerL
-		unsigned char  triggerR
-		unsigned char  analogA
-		unsigned char  analogB
-		signed   char  err
-	
-	unsigned int PAD_Read(PADStatus*)
-	unsigned int PAD_Init()
-	unsigned int PAD_ControlMotor(int pad, unsigned int cmd)
+cdef extern from "wiiuse/wpad.h":
+	signed long WPAD_Init()
+	signed long WPAD_DroppedEvents(signed long chan)
+	signed long WPAD_Flush(signed long chan)
+	signed long WPAD_SetDataFormat(signed long chan, signed long fmt)
+	signed long WPAD_SetVRes(signed long chan,unsigned long xres,unsigned long yres)
+	signed long WPAD_GetStatus()
+	signed long WPAD_Probe(signed long chan,unsigned long *type)
+	signed long WPAD_Disconnect(signed long chan)
+	void WPAD_Shutdown()
+	void WPAD_SetIdleTimeout(unsigned long seconds)
+	signed long WPAD_ScanPads()
+	signed long WPAD_Rumble(signed long chan, int status)
+	signed long WPAD_SetIdleThresholds(signed long chan, signed long btns, signed long ir, signed long accel, signed long js)
+	unsigned long WPAD_ButtonsUp(int chan)
+	unsigned long WPAD_ButtonsDown(int chan)
+	unsigned long WPAD_ButtonsHeld(int chan)
 
-BUTTON_LEFT  =	0x0001
-BUTTON_RIGHT =	0x0002
-BUTTON_DOWN  =	0x0004
-BUTTON_UP    =	0x0008
-TRIGGER_Z    =	0x0010
-TRIGGER_R    =	0x0020
-TRIGGER_L    =	0x0040
-BUTTON_A     =	0x0100
-BUTTON_B     =	0x0200
-BUTTON_X     =	0x0400
-BUTTON_Y     =	0x0800
-BUTTON_MENU  =	0x1000
-BUTTON_START =	0x1000
+WPAD_CHAN_ALL = -1,
+WPAD_CHAN_0=0
+WPAD_CHAN_1=1
+WPAD_CHAN_2=2
+WPAD_CHAN_3=3
+WPAD_MAX_WIIMOTES=4
+											
+WPAD_BUTTON_2=							0x0001
+WPAD_BUTTON_1=							0x0002
+WPAD_BUTTON_B=							0x0004
+WPAD_BUTTON_A	=						0x0008
+WPAD_BUTTON_MINUS=						0x0010
+WPAD_BUTTON_HOME=						0x0080
+WPAD_BUTTON_LEFT=						0x0100
+WPAD_BUTTON_RIGHT=						0x0200
+WPAD_BUTTON_DOWN=						0x0400
+WPAD_BUTTON_UP=							0x0800
+WPAD_BUTTON_PLUS=						0x1000
+WPAD_BUTTON_UNKNOWN=						0x8000
+											
+WPAD_NUNCHUK_BUTTON_Z=					(0x0001<<16)
+WPAD_NUNCHUK_BUTTON_C=					(0x0002<<16)
+											
+WPAD_CLASSIC_BUTTON_UP=					(0x0001<<16)
+WPAD_CLASSIC_BUTTON_LEFT=				(0x0002<<16)
+WPAD_CLASSIC_BUTTON_ZR=					(0x0004<<16)
+WPAD_CLASSIC_BUTTON_X=					(0x0008<<16)
+WPAD_CLASSIC_BUTTON_A=					(0x0010<<16)
+WPAD_CLASSIC_BUTTON_Y=					(0x0020<<16)
+WPAD_CLASSIC_BUTTON_B=					(0x0040<<16)
+WPAD_CLASSIC_BUTTON_ZL=					(0x0080<<16)
+WPAD_CLASSIC_BUTTON_FULL_R=				(0x0200<<16)
+WPAD_CLASSIC_BUTTON_PLUS=				(0x0400<<16)
+WPAD_CLASSIC_BUTTON_HOME=				(0x0800<<16)
+WPAD_CLASSIC_BUTTON_MINUS=				(0x1000<<16)
+WPAD_CLASSIC_BUTTON_FULL_L=				(0x2000<<16)
+WPAD_CLASSIC_BUTTON_DOWN=				(0x4000<<16)
+WPAD_CLASSIC_BUTTON_RIGHT=				(0x8000<<16)
 
-buttons = ['Left', 'Right', 'Down', 'Up', 'Start',
-		   'A', 'B', 'X', 'Y', 'Z', 'L', 'R']
+WPAD_GUITAR_HERO_3_BUTTON_STRUM_UP=		(0x0001<<16)
+WPAD_GUITAR_HERO_3_BUTTON_YELLOW=		(0x0008<<16)
+WPAD_GUITAR_HERO_3_BUTTON_GREEN=			(0x0010<<16)
+WPAD_GUITAR_HERO_3_BUTTON_BLUE=			(0x0020<<16)
+WPAD_GUITAR_HERO_3_BUTTON_RED=			(0x0040<<16)
+WPAD_GUITAR_HERO_3_BUTTON_ORANGE=		(0x0080<<16)
+WPAD_GUITAR_HERO_3_BUTTON_PLUS=			(0x0400<<16)
+WPAD_GUITAR_HERO_3_BUTTON_MINUS=			(0x1000<<16)
+WPAD_GUITAR_HERO_3_BUTTON_STRUM_DOWN=	(0x4000<<16)
+
+
+buttons = ['2', '1', 'B', 'A', '-','+','HOME','LEFT','RIGHT','DOWN','UP','PLUS','UNKNOWN']
 
 ERR_NONE          =	 0
 ERR_NO_CONTROLLER =	-1
 ERR_NOT_READY     =	-2
 ERR_TRANSFER      =	-3
 
+
 # Call PAD_Init whenever this module is inited
-PAD_Init()
+WPAD_Init()
 
-cdef PADStatus pads[4]
-cdef int _need2read 
-cdef int _lastUpdate
-_need2read = 1
-_lastUpdate = 0
-
-def SetReadFlag():
-	global _need2read
-	_need2read = 1
-
-class PAD:
+class WPAD:
 	"""
-	PAD class, let you get info from, and control a specific GC controller
-	Initialize the object with the pad number
+	WPAD class, let you get info from, and control a specific wii controller
+	Initialize the object with the channel number
 	Button/Stick states can be accessed by padObj['A'] or padObj['LStick']
 	"""
-	def __init__(self, padNum):
-		self.padNum = padNum
-		self.lastUpdate = -1
+	def __init__(self, chanNum):
+		self.chanNum = chanNum
+		self.buttons = 0
 	def __getitem__(self, b):
-		global _need2read, _lastUpdate, pads
-		if _need2read:
-			PAD_Read(pads)
-			_need2read = 0
-			_lastUpdate = _lastUpdate+1
-		
-		if self.lastUpdate != _lastUpdate:
-			self._update()
-			self.lastUpdate = _lastUpdate
-		
 		return self._dict[b]
 	
-	def _update(self):
+	def update(self):
 		# Called to update the pad state
-		global pads
-		cdef PADStatus pad
-		pad = pads[self.padNum]
-		self._dict = { 'A'      : pad.button & BUTTON_A,
-					   'B'      : pad.button & BUTTON_B,
-					   'X'      : pad.button & BUTTON_X,
-					   'Y'      : pad.button & BUTTON_Y,
-					   'Z'      : pad.button & TRIGGER_Z,
-					   'Start'  : pad.button & BUTTON_START,
-					   'Up'     : pad.button & BUTTON_UP,
-					   'Down'   : pad.button & BUTTON_DOWN,
-					   'Left'   : pad.button & BUTTON_LEFT,
-					   'Right'  : pad.button & BUTTON_RIGHT,
-					   'L'      : pad.button & TRIGGER_L,
-					   'R'      : pad.button & TRIGGER_R,
-					   'AnalogL': pad.triggerL,
-					   'AnalogR': pad.triggerR,
-					   'AnalogA': pad.analogA,
-					   'AnalogB': pad.analogB,
-					   'LStick' : ( pad.stickX, pad.stickY ),
-					   'RStick' : ( pad.substickX, pad.substickY ),
-					   'Error'  : pad.err }
+		self.buttons = WPAD_ButtonsDown(self.chan)
+		self._dict = { 'A'      : pad.button & WPAD_BUTTON_A,
+			       'B'      : pad.button & WPAD_BUTTON_B,
+			       '-'      : pad.button & WPAD_BUTTON_MINUS,
+			       '+'      : pad.button & WPAD_BUTTON_PLUS,
+			       'HOME'      : pad.button & WPAD_BUTTON_HOME,
+			       '2'  : pad.button & WPAD_BUTTON_2,
+			       '1'  : pad.button & WPAD_BUTTON_2,
+			       'Up'     : pad.button & WPAD_BUTTON_UP,
+			       'Down'   : pad.button & WPAD_BUTTON_DOWN,
+			       'Left'   : pad.button & WPAD_BUTTON_LEFT,
+			       'Right'  : pad.button & WPAD_BUTTON_RIGHT
+			       }
 		
 	
 	def rumble_start(self):
-		PAD_ControlMotor(self.padNum, 1)
+		WPAD_Rumble(self.chanNum, 1)
 	def rumble_stop(self):
-		PAD_ControlMotor(self.padNum, 0)
+		WPAD_Rumble(self.chanNum, 0)
 	def rumble_stop_hard(self):
-		PAD_ControlMotor(self.padNum, 2)
+		WPAD_Rumble(self.chanNum, 2)
 
